6650dda7922832996275ba4e59b920ac2bc3a974
xiongbin <923450980@qq.com>
2021-01-15 17:30:13 +0800

    first commit

---
+++ bt_test.ino
@@ -0,0 +1,48 @@
+#define LED 13
+
+char json[] = "{\"sensor\":\"gps\",\"time\":1351824120,\"data\":[48.756080,2.302038]}";
+
+DynamicJsonDocument doc(1024);
+deserializeJson(doc, json);
+
+const char* sensor = doc["sensor"];
+long time          = doc["time"];
+double latitude    = doc["data"][0];
+double longitude   = doc["data"][1];
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (stringComplete) {
+    Serial.println(inputString);
+    
+    // clear the string:
+    inputString = "";
+    stringComplete = false;
+  }
+}
+
+void parseData(){
+  
+}
+
+
+
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
+}
